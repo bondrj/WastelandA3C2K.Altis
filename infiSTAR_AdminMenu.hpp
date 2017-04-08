c1151ab7b73c7335c6a0e862828fcb3fac7785f5
C2K_FliCK <flick@clanc2k.com.br>
2017-04-08 18:25:20 -0300

    Update do Infistar

--- infiSTAR_AdminMenu.hpp
+++ infiSTAR_AdminMenu.hpp
@@ -2,8 +2,8 @@
 Antihack & AdminTools - Christian Lorenzen - www.infiSTAR.de
 
 infiSTAR (for) A3 #2967
-Date: 21-Mar-2017 06-10-09
-Version: v0247
+Date: 02-Apr-2017 00-14-36
+Version: v0248
 Owner: leandro_santos-rj@hotmail.com
 OwnerUID: 76561197995894355
 */
